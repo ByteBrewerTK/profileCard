5051798bcfd73a2f0a8914d212debbd7cb99b480
abutalif <105064795+talifkhan22222@users.noreply.github.com>
2023-02-27 21:28:23 +0530

    2nd/pupupModal/with copy path opt

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ const closeModal = ()=>{
     modal.classList.remove("active-modal");
     overlay.classList.remove("overlay-active");
 };
+
+let copyText = document.querySelector(".copy-box");
+document.querySelector(".copy-btn").addEventListener('click', function(){
+    let input = document.querySelector('.text');
+    input.select();
+    document.execCommand('copy');
+    copyText.classList.add("active-pop");
+    window.getSelection().removeAllRanges();
+    setTimeout(function(){
+        copyText.classList.remove("active-pop");
+    }, 2500);
+});
